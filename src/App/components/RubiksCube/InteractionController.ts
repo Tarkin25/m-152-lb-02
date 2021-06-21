fb7afe20869d2f54076098c0c05bc12af07aa625
Tarkin25 <severin.weigold@edu.tbz.ch>
2021-06-21 22:52:56 +0200

    remove planehelper

--- src/App/components/RubiksCube/InteractionController.ts
+++ src/App/components/RubiksCube/InteractionController.ts
@@ -1,4 +1,9 @@
-import { Camera, LineSegments, Object3D, Plane, PlaneHelper, Raycaster, Vector2, Vector3 } from "three";
+import {
+    Camera,
+    LineSegments,
+    Object3D, Raycaster,
+    Vector2
+} from "three";
 import { Piece } from "./Piece";
 import { Axis } from "./RubiksCube";
 
@@ -11,20 +16,16 @@ export class InteractionController {
     private raycaster: Raycaster;
     private mouse: Vector2;
     private plane: WhackPlane | undefined;
-    private planeHelper: PlaneHelper;
 
     constructor(
         private pieces: Piece[],
         private container: HTMLElement,
         private camera: Camera,
-        private target: Object3D,
+        private target: Object3D
     ) {
         this.raycaster = new Raycaster();
         this.mouse = new Vector2();
         this.plane = undefined;
-        this.planeHelper = new PlaneHelper(new Plane(), 4, 0xff0000);
-        this.planeHelper.visible = false;
-        this.target.add(this.planeHelper);
 
         container.addEventListener("mousemove", (e) => {
             this.onMouseMove(e);
@@ -33,11 +34,11 @@ export class InteractionController {
 
         container.addEventListener("contextmenu", () => {
             this.onClick();
-        })
+        });
 
         container.addEventListener("click", () => {
             this.onContextMenu();
-        })
+        });
     }
 
     onTurnLeft(_plane: WhackPlane) {}
@@ -51,7 +52,7 @@ export class InteractionController {
     }
 
     private onContextMenu() {
-        if(this.plane) {
+        if (this.plane) {
             this.onTurnRight(this.plane);
         }
     }
@@ -71,16 +72,20 @@ export class InteractionController {
                 const object = intersect.object as LineSegments;
                 const piece = object.parent! as Piece;
                 //piece.setHovered(true);
-                
-                const normal = intersect.face.normal.clone().applyQuaternion(piece.quaternion).round();
-                
-                const axis = Object.entries(normal).filter(([_key, value]) => Math.abs(value) !== 0)[0][0] as Axis;
+
+                const normal = intersect.face.normal
+                    .clone()
+                    .applyQuaternion(piece.quaternion)
+                    .round();
+
+                const axis = Object.entries(normal).filter(
+                    ([_key, value]) => Math.abs(value) !== 0
+                )[0][0] as Axis;
                 const index = piece.position[axis];
 
-                this.plane = {axis, index};
+                this.plane = { axis, index };
                 this.hoverPlane(axis, index);
             }
-
         } else {
             this.plane = undefined;
         }
@@ -92,18 +97,6 @@ export class InteractionController {
     }
 
     private hoverPlane(axis: Axis, index: number) {
-        const normal = new Vector3();
-        normal[axis] = index;
-        const swap = normal.x;
-        normal.x = normal.z;
-        normal.z = swap;
-
-        this.planeHelper.plane.normal = normal;
-        this.planeHelper.plane.constant = -1;
-        this.planeHelper.visible = true;
-
-        console.log(this.planeHelper.plane);
-
         this.pieces.forEach((piece) => {
             if (Math.abs(piece.position[axis] - index) <= 0.01) {
                 piece.setHovered(true);
